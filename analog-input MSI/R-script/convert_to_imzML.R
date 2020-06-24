c0796ee404e690a146e9338cd4756fa97ab5e87a
Benjamin Bartels <mail.benjamin.bartels@gmail.com>
2020-06-24 17:48:01 +0200

    Update convert_to_imzML.R

--- analog-input MSI/R-script/convert_to_imzML.R
+++ analog-input MSI/R-script/convert_to_imzML.R
@@ -2,13 +2,12 @@
 library(MALDIquant)
 library(MALDIquantForeign)
 library(viridis)
-library(RmsiGUI)
 
 # with MassWolf RAW to mzXML (in windows console!)
-massWolf --mzXML C:\MassLynx\Default.pro\Data\file.raw C:\MassLynx\Default.pro\Data\file.mzXML
+massWolf --mzXML C:\MassLynx\Example.pro\Data\file.raw C:\MassLynx\Example.pro\Data\file.mzXML
 
 ## mzXML to imzML
-# Enter sample name (without spaces, example: HylemonellaMono)
+# Enter sample name (without spaces)
 # Enter path for analog data file (*.txt)
 # Enter path for TIC data file (*.txt)
 # Enter path for mzXML data file (*.mzXML)
@@ -17,26 +16,19 @@ massWolf --mzXML C:\MassLynx\Default.pro\Data\file.raw C:\MassLynx\Default.pro\D
 # Enter the number of replicates on the plate for a single acquisition(integer)
 #e.g.:
 RAW_to_ImZML()
-180720_NEG_Thaliana_Leaf_bottom_2_Pierced_1249
-W:\Data_Synapt\2018\zzhaoyu\mzXML\180720_NEG_Thaliana_Leaf_bottom_2_Pierced_1249_Analog.txt
-W:\Data_Synapt\2018\zzhaoyu\mzXML\180720_NEG_Thaliana_Leaf_bottom_2_Pierced_1249_TIC.txt
-W:\Data_Synapt\2018\zzhaoyu\mzXML\180720_NEG_Thaliana_Leaf_bottom_2_Pierced_1249_51x26.mzXML
+180720_NEG_imaging_plant_leaf_2_treated_0815
+W:\Data_Synapt\2020\musterfrau\mzXML\180720_NEG_imaging_plant_leaf_2_treated_0815_Analog.txt
+W:\Data_Synapt\2020\musterfrau\mzXML\180720_NEG_imaging_plant_leaf_2_treated_0815_TIC.txt
+W:\Data_Synapt\2020\musterfrau\mzXML\180720_NEG_imaging_plant_leaf_2_treated_0815_51x26.mzXML
 51
 26
 1
 
 # visualizing ion maps from imzML
 library(viridis)
-data_treat <- importImzMl("F:/Dokumente/Daten/Arabidopsis/imzML/180810_NEG_Thaliana_Leaf_bottom_4_Pierced_1543.imzML")
-data_control <- importImzMl("F:/Dokumente/Daten/Arabidopsis/imzML/180810_NEG_Thaliana_Leaf_bottom_3_1419.imzML")
-data_cotton <- importImzMl("F:/Dokumente/Daten/Cotton/180418_NEG_Cotton_Leaf_1//180418_NEG_Cotton_wrong.imzML")
-data_cotton_alt <- importImzMl("W:/Data_Synapt/2018/bbartels/mzXML/imzML/180418_NEG_Cotton_Leaf_new.imzML")
+data_treat <- importImzMl("W:/Data_Synapt/2020/musterfrau/imzML/180720_NEG_imaging_plant_leaf_2_treated_0815.imzML")
+data_control <- importImzMl("W:/Data_Synapt/2020/musterfrau/imzML/180720_NEG_imaging_plant_leaf_1_cntrl_1337.imzML")
 
 pic_treat <- plotMsiSlice(data_treat, center = 436.04, tolerance = 0.05, colorRamp(viridis(1000)), legend = FALSE)
 pic_control <- plotMsiSlice(data_control, center = 436.04, tolerance = 0.05, colorRamp(viridis(1000)), legend = FALSE)
-pic_test <- plotMsiSlice(data_cotton, center = c(273.09), tolerance = 0.01, colorRamp(viridis(100)), legend = TRUE)
-
-## alternatively RmsiGUI from Dr. Robert Winkler
-library(RmsiGUI)
-launch()
 
